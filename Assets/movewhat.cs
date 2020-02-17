65823c55cd4532e3552f18498d74fdb83ef7e383
JiJSaS <nikand8121@gmail.com>
2020-02-17 22:50:50 +0300

    Initial commit

---
+++ Assets/movewhat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class movewhat : MonoBehaviour
+{
+    public FixedJoystick leftJoystick;
+    public FixedButton Button;
+    protected ThirdPersonUserControl ctrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ctrl = GetComponent<ThirdPersonUserControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ctrl.m_Jump = Button.Pressed;
+        ctrl.HInput = leftJoystick.Horizontal;
+        ctrl.VInput = leftJoystick.Vertical;
+    }
+}
